046fdab7d1fb0392bd0bf485a437727006ab2a0f
kang <codekxx@163.com>
2019-07-21 17:46:28 +0800

    首次提交项目

---
+++ lib/pages/category_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CateGoryPage extends StatefulWidget {
+  @override
+  _CateGoryPageState createState() => _CateGoryPageState();
+}
+
+class _CateGoryPageState extends State<CateGoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('分类')
+      )
+    );
+  }
+}
